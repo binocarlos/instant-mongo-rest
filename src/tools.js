06ac640392199c6ec4224072ec45e8692ee3b09f
Kai Davenport <kaiyadavenport@gmail.com>
2016-12-07 12:06:25 +0000

    re-connect to mongo

--- src/tools.js
+++ src/tools.js
@@ -4,9 +4,37 @@ mongoose.Promise = require('bluebird')
 
 function connectToMongo(args){
   var connectionUrl = 'mongodb://' + args.host + ':' + args.port + '/' + args.database
-  console.log('-------------------------------------------');
-  console.log(connectionUrl)
-  mongoose.connect(connectionUrl)
+
+  var isConnectedBefore = false;
+  function connect() {
+      mongoose.connect(connectionUrl, {server: { auto_reconnect: true }})
+  }
+  connect()
+
+  mongoose.connection.on('error', function() {
+      console.log('Could not connect to MongoDB')
+  })
+
+  mongoose.connection.on('disconnected', function(){
+    console.log('Lost MongoDB connection...')
+    if (!isConnectedBefore) connect()
+  })
+  mongoose.connection.on('connected', function() {
+    isConnectedBefore = true
+    console.log('Connection established to MongoDB')
+  })
+
+  mongoose.connection.on('reconnected', function() {
+    console.log('Reconnected to MongoDB')
+  })
+
+  // Close the Mongoose connection, when receiving SIGINT
+  process.on('SIGINT', function() {
+    mongoose.connection.close(function () {
+      console.log('Force to close the MongoDB conection')
+      process.exit(0)
+    })
+  })
 }
 
 module.exports = {
